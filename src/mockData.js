6c4b2beaeac42ba4f2b89a868527f8271d9faf4a
Matthieu Nogueron <matthieu@worldfavor.com>
2019-07-05 17:03:30 +0200

    Add mock data and design

---
+++ src/mockData.js
@@ -0,0 +1,42 @@
+import SystembolagetLogo from './assets/Systembolaget_logo.jpg'
+import PolarbrodLogo from './assets/Polarbrod_logo.jpg'
+import SasLogo from './assets/Sas_logo.jpg'
+import FolksamLogo from './assets/Folksam_logo.png'
+
+import SystembolagetBackground from './assets/Systembolaget_background.jpg'
+import PolarbrodBackground from './assets/Polarbrod_background.jpg'
+import SasBackground from './assets/Sas_background.jpg'
+import FolksamBackground from './assets/Folksam_background.png'
+
+export default {
+  data: [
+    {
+      guid: '3c8d1cff-b58c-44c5-a684-92fbcb95a0c3',
+      name: 'Systembolaget',
+      imageUrl: SystembolagetLogo,
+      backgroundImageUrl: SystembolagetBackground,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a viverra diam, convallis tempor risus. Phasellus eget tempus tortor, ut pellentesque lorem. \nProin rutrum sagittis nisl, vel semper turpis maximus ac. Ut facilisis enim ut neque congue volutpat quis volutpat ex. \nVivamus luctus ex a consequat posuere. Fusce non mollis mi, vitae lacinia ligula. Donec nec libero mi. Nullam sit amet pretium neque. Nunc facilisis fermentum malesuada. Curabitur egestas eget velit eu mattis. Morbi vitae neque eu nunc feugiat ullamcorper. In magna nisl, feugiat a est maximus, molestie euismod nibh.',
+    },
+    {
+      guid: '8bfc1837-7a8f-4ea3-8ede-26d9c23f112b',
+      name: 'Polarbröd',
+      imageUrl: PolarbrodLogo,
+      backgroundImageUrl: PolarbrodBackground,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a viverra diam, convallis tempor risus. Phasellus eget tempus tortor, ut pellentesque lorem.',
+    },
+    {
+      guid: '2ead09f1-a2e3-41e4-a8ea-f08d123819b1',
+      name: 'Scandinavian Airline Systems',
+      imageUrl: SasLogo,
+      backgroundImageUrl: SasBackground,
+      description: '',
+    },
+    {
+      guid: '643ce21b-8314-4c03-b577-50aa1111943a',
+      name: 'Folksam',
+      imageUrl: FolksamLogo,
+      backgroundImageUrl: FolksamBackground,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a viverra diam, convallis tempor risus. Phasellus eget tempus tortor, ut pellentesque lorem. \nProin rutrum sagittis nisl, vel semper turpis maximus ac. Ut facilisis enim ut neque congue volutpat quis volutpat ex. \nVivamus luctus ex a consequat posuere. Fusce non mollis mi, vitae lacinia ligula. Donec nec libero mi. Nullam sit amet pretium neque. Nunc facilisis fermentum malesuada. Curabitur egestas eget velit eu mattis. Morbi vitae neque eu nunc feugiat ullamcorper. In magna nisl, feugiat a est maximus, molestie euismod nibh.',
+    },
+  ]
+}
