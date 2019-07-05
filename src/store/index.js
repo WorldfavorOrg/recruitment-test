ced93ab5087d536c5a98025232d6d97f1d0144cd
Matthieu Nogueron <matthieu@worldfavor.com>
2019-07-05 16:00:31 +0200

    Clenup unused imports

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 import reducers from '../reducers'
 
 export const initStore = () => {
